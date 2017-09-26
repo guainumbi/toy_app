abb01f68ff600b3594859f80d127b8547dfecd39
Jessica Grzanna <jgrzanna@me.com>
2017-09-26 11:49:15 +0200

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(name: 'Jess', email: 'neue@email.com')
+User.create(name: "Nik", email: "seine@email.de")
+User.create(name: "Kitty", email: "cat@email.de")
